7e0574fb596c1425a5aa003caec61afd9a040c1d
Oguz Ozsen <aslanoguz_1101@hotmail.com>
2021-09-04 21:02:02 +0300

    Contextle Sarmalandı Bootstrap importlandı

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Control from "./Control";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Control>
+      <App />
+    </Control>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
