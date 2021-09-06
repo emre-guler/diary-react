e8a1c6dd8ea20e4f748bef6ee9260becb6264303
ExoComfix <57865900+ExoComfix@users.noreply.github.com>
2021-09-06 22:07:16 +0300

    NavBar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import { Button } from "react-bootstrap";
 const Header = () => {
     return (
-        <header>
-            KAFA
+        <header className="Header">
+            {/* Header Logo */}
+            <div className="logo">
+                <a href="/">Ionic Diary</a>
+            </div>
+            {/* Header Links */}
+            <div className="headerLinks">
+                < a href="/">Anasayfa</a>
+                < a href="/">İletişim</a>
+                < a href="/">Hakkımzıda</a>
+            </div>
+            {/* Sign Buttons */}
+            <div className="signButtons">
+                <Button variant = "light">Kayıt Ol</Button>
+                <Button variant = "primary">Giriş Yap</Button>
+            </div>
         </header>
-    )
-}
-
+    );
+};
 export default Header
