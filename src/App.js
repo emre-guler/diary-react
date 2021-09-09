614a54c55fdb4c7344b6f2f6170821563e31a31e
Oguz Ozsen <aslanoguz_1101@hotmail.com>
2021-09-09 22:49:54 +0300

    Add App.js React Router and Some Responsive Edits

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import Header from "./Components/Header"
-import Content from "./Components/Content"
-import Footer from "./Components/Footer"
+import Header from "./Components/Header";
+import Content from "./Components/Content";
+import Footer from "./Components/Footer";
 
-import "./assets/styleHeader.css"
-import "./assets/styleContent.css"
-import "./assets/styleFooter.css"
+import "./assets/styleHeader.css";
+import "./assets/styleContent.css";
+import "./assets/styleFooter.css";
+
+import { BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Content/>
-      <Footer/>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Content.js
+++ src/Components/Content.js
@@ -7,7 +7,7 @@ const Content = () => {
   return (
     <section>
       <Row className="contentNotLogin">
-        <Col className="contentText" xl={7} md={6} sm={6}>
+        <Col className="contentText" xl={7} md={6} sm={6} xs={8}>
           <h1>Kendi sanal günlüğünüzü oluşturun.</h1>
           <span>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
@@ -16,20 +16,20 @@ const Content = () => {
             pretium velit.
           </span>
         </Col>
-        <Col className="contentPicture" xl={5} md={6} sm={6}>
+        <Col className="contentPicture" xl={5} md={5} sm={6} xs={8}>
           <img src={logo} alt="Kitap" />
         </Col>
       </Row>
       <Row className="contentPropRow">
-        <Col className="contentPropHead" xl={8} md={8} sm={8}>
+        <Col className="contentPropHead mt-5" xl={8} md={8} sm={8} xs={8}>
           <h2>Günlük Özellikleri</h2>
           <span>
             Sanal günlüğünüzü özelliştirebilmeniz için bir kaç seçenek.
           </span>
         </Col>
-        <Col className="contentProps" xl={8} md={8} sm={8}>
-          <Col xl={3} md={3} sm={3}>
-            <Card style={{ width: "18rem", border: "none" }}>
+        <Col className="contentProps" xl={8} md={8} sm={8} xs={8}>
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Card style={{border: "none" }}>
               <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">
                   <span className="propPicBackground">
@@ -40,14 +40,15 @@ const Content = () => {
                 </Card.Subtitle>
                 <Card.Title>Fotoğraflar</Card.Title>
                 <Card.Text>
-                  Sanal günlüğünüze, kaydettiğiniz fotoğrafları koyup
-                  kişiselleştirebilirsiniz.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            egestas magna aliquet nulla mollis gravida. Aenean cursus, quam id
+            gravida porta, mi augue porttitor nisi.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col xl={3} md={3} sm={3}>
-            <Card style={{ width: "18rem", border: "none" }}>
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Card style={{border: "none" }}>
               <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">
                   <span className="propPicBackground">
@@ -58,14 +59,15 @@ const Content = () => {
                 </Card.Subtitle>
                 <Card.Title>Fotoğraflar</Card.Title>
                 <Card.Text>
-                  Sanal günlüğünüze, kaydettiğiniz fotoğrafları koyup
-                  kişiselleştirebilirsiniz.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            egestas magna aliquet nulla mollis gravida. Aenean cursus, quam id
+            gravida porta, mi augue porttitor nisi.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col xl={3} md={3} sm={3}>
-            <Card style={{ width: "18rem", border: "none" }}>
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Card style={{border: "none" }}>
               <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">
                   <span className="propPicBackground">
@@ -76,14 +78,15 @@ const Content = () => {
                 </Card.Subtitle>
                 <Card.Title>Fotoğraflar</Card.Title>
                 <Card.Text>
-                  Sanal günlüğünüze, kaydettiğiniz fotoğrafları koyup
-                  kişiselleştirebilirsiniz.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            egestas magna aliquet nulla mollis gravida. Aenean cursus, quam id
+            gravida porta, mi augue porttitor nisi.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col xl={3} md={3} sm={3}>
-            <Card style={{ width: "18rem", border: "none" }}>
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Card style={{border: "none" }}>
               <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">
                   <span className="propPicBackground">
@@ -94,14 +97,15 @@ const Content = () => {
                 </Card.Subtitle>
                 <Card.Title>Fotoğraflar</Card.Title>
                 <Card.Text>
-                  Sanal günlüğünüze, kaydettiğiniz fotoğrafları koyup
-                  kişiselleştirebilirsiniz.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            egestas magna aliquet nulla mollis gravida. Aenean cursus, quam id
+            gravida porta, mi augue porttitor nisi.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col xl={3} md={3} sm={3}>
-            <Card style={{ width: "18rem", border: "none" }}>
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Card style={{border: "none" }}>
               <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">
                   <span className="propPicBackground">
@@ -112,14 +116,15 @@ const Content = () => {
                 </Card.Subtitle>
                 <Card.Title>Fotoğraflar</Card.Title>
                 <Card.Text>
-                  Sanal günlüğünüze, kaydettiğiniz fotoğrafları koyup
-                  kişiselleştirebilirsiniz.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            egestas magna aliquet nulla mollis gravida. Aenean cursus, quam id
+            gravida porta, mi augue porttitor nisi.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col xl={3} md={3} sm={3}>
-            <Card style={{ width: "18rem", border: "none" }}>
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Card style={{border: "none" }}>
               <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">
                   <span className="propPicBackground">
@@ -130,14 +135,15 @@ const Content = () => {
                 </Card.Subtitle>
                 <Card.Title>Fotoğraflar</Card.Title>
                 <Card.Text>
-                  Sanal günlüğünüze, kaydettiğiniz fotoğrafları koyup
-                  kişiselleştirebilirsiniz.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            egestas magna aliquet nulla mollis gravida. Aenean cursus, quam id
+            gravida porta, mi augue porttitor nisi.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col xl={3} md={3} sm={3}>
-            <Card style={{ width: "18rem", border: "none" }}>
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Card style={{border: "none" }}>
               <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">
                   <span className="propPicBackground">
@@ -148,14 +154,15 @@ const Content = () => {
                 </Card.Subtitle>
                 <Card.Title>Fotoğraflar</Card.Title>
                 <Card.Text>
-                  Sanal günlüğünüze, kaydettiğiniz fotoğrafları koyup
-                  kişiselleştirebilirsiniz.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            egestas magna aliquet nulla mollis gravida. Aenean cursus, quam id
+            gravida porta, mi augue porttitor nisi.
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col xl={3} md={3} sm={3}>
-            <Card style={{ width: "18rem", border: "none" }}>
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Card style={{border: "none" }}>
               <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">
                   <span className="propPicBackground">
