7d807dfe9e1fb6d54e992f5b13bd613497178e75
Oguz Ozsen <aslanoguz_1101@hotmail.com>
2021-09-14 19:16:36 +0300

    Header Changes and Responsive Button Add

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,29 @@
-import React from 'react'
+import React, {useState} from "react";
 import { Button } from "react-bootstrap";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 const Header = () => {
-    return (
-        <header className="header-container">
-            {/* Header Logo */}
-            <div className="logo">
-                <a href="/">Ionic Diary</a>
-            </div>
-            {/* Header Links */}
-            <div className="headerLinks">
-                <a href="/">Anasayfa</a>
-                <a href="/">İletişim</a>
-                <a href="/">Hakkımzıda</a>
-            </div>
-            {/* Sign Buttons */}
-            <div className="signButtons">
-                <Button variant = "light">Kayıt Ol</Button>
-                <Button variant = "primary">Giriş Yap</Button>
-            </div>
-        </header>
-    );
+    const [responsive,setResponsive] = useState(false);
+  return (
+    <header className="header-container">
+      {/* Header Logo */}
+      <div className="logo">
+        <a href="/">Ionic Diary</a>
+      </div>
+      {/* Header Links */}
+      <div className="headerLinks">
+        <a href="/">Anasayfa</a>
+        <a href="/">İletişim</a>
+        <a href="/">Hakkımızda</a>
+      </div>
+      {/* Sign Buttons */}
+      <div className="signButtons">
+        <Button variant="light">Kayıt Ol</Button>
+        <Button variant="primary">Giriş Yap</Button>
+      </div>
+      <div className="responsiveMenu" onClick={()=>{setResponsive(!responsive)}}>
+        <HiOutlineDotsVertical className={responsive ? "responsiveMenuDots responsiveActive":"responsiveMenuDots"} />
+      </div>
+    </header>
+  );
 };
-export default Header
+export default Header;
