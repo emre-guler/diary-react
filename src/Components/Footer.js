784a884c1c013d046227bca79862f3687581a36d
Oguz Ozsen <aslanoguz_1101@hotmail.com>
2021-09-15 20:19:52 +0300

    add footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,54 +1,78 @@
-import React from 'react'
-import { Button } from "react-bootstrap";
+import React from "react";
+import { Button, Row, Col } from "react-bootstrap";
+import {FaInstagram, FaTwitter} from "react-icons/fa"
 
 const Footer = () => {
-    return (
-        <footer className="footer-container">
-            <div className="footer-logo">
-                <a href="/">Ionic</a>
-            </div>
-            <br></br>
-            <br></br>
-            <div className="row footer-parts">
-                <div className="footer-part col-12 col-sm-6 col-md-3">
-                    <h5 className="fw-bold header-text footer-part-header" href="/">Hakkımızda</h5>
-                    <a className="footer-part-item" href="/">Ekip</a>
-                    <br></br>
-                    <a className="footer-part-item" href="/">Kariyer</a>
-                    <br></br>
-                    <a className="footer-part-item" href="/">Vizyonumuz</a>
-                    <br></br>
-                </div>
-                <div className="footer-part col-12 col-sm-6 col-md-3">
-                    <h5 className="fw-bold header-text footer-part-header" href="/">Destek</h5>
-                    <a className="footer-part-item" href="/">Ekip</a>
-                    <br></br>
-                    <a className="footer-part-item" href="https://discord.gg/DKSZwhEsys">Discord</a>
-                    <br></br>
-                </div>
-                <div className="footer-part col-12 col-sm-6 col-md-3">
-                    <h5 className="fw-bold header-text footer-part-header" href="/">İletişim</h5>
-                    <a className="footer-part-item" href="/">Basın</a>
-                    <br></br>
-                    <a className="footer-part-item" href="/">Partnerlikler</a>
-                    <br></br>
-                </div>
-            </div>
-            <div className="footer-cr-area row">
-                <div className="cr-text col-6 col-md-4 text-center text-md-start">
-                    <div className="text-muted copyright">© 2021 Ionic. Digital Diary</div>
-                    <div className="text-muted createdby">Teknoloji manyakları tarafından günlük tutmayı severler için yapıldı.</div>
-                </div>
-                <div className="cr-text col-6 col-md-4 text-center text-md-start">
-                    <Button variant = "primary">Şimdi Kaydol</Button>
-                </div>
-            </div>
-            <div className="footer-sc-lght-area row">
-                <a className="lght-txt text-muted col-12 col-md-8 text-center text-md-start" href="/kvkk">Aydınlatma Metni</a>
-                <br></br>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="Footer">
+      {/* footer logo */}
+      <Row className="footerLogoRow">
+        <Col xl={8} md={8} sm={8} xs={8}>
+          <Col>
+            <a href="/" className="footerLogo">
+              Ionic Diary
+            </a>
+          </Col>
+        </Col>
+      </Row>
+      {/* footer links */}
+      <Row>
+        <Col xl={8} md={8} sm={8} xs={8} className="footerLinksCol">
+          <Col xl={3} md={3} sm={3} xs={3}>
+            <h5>Ürünler</h5>
+            <a href="/">Lorem</a>
+            <a href="/">Ipsum Dolor</a>
+            <a href="/">Dolor Sit Amet</a>
+            <a href="/">Sova</a>
+          </Col>
+          <Col xl={3} md={3} sm={3} xs={3}>
+            <h5>Scoutli Hakkında</h5>
+            <a href="/">Lorem</a>
+            <a href="/">Ipsum Dolor</a>
+            <a href="/">Dolor Sit Amet</a>
+            <a href="/">Sova</a>
+          </Col>
+          <Col xl={3} md={3} sm={3} xs={3}>
+            <h5>Destek</h5>
+            <a href="/">Lorem</a>
+            <a href="/">Ipsum Dolor</a>
+            <a href="/">Dolor Sit Amet</a>
+            <a href="/">Sova</a>
+          </Col>
+          <Col xl={3} md={3} sm={3} xs={3}>
+            <h5>İletişim</h5>
+            <a href="/">Lorem</a>
+            <a href="/">Ipsum Dolor</a>
+            <a href="/">Dolor Sit Amet</a>
+            <a href="/">Sova</a>
+          </Col>
+        </Col>
+      </Row>
+      {/* footer copyright */}
+      <Row>
+        <Col xl={8} md={8} sm={8} xs={8} className="footerCopyrightCol">
+          <Col xl={8} md={8} sm={8} xs={8} className="footerCopyright">
+            <p className="text-muted">
+              © 2021 Scoutli. Make Esports Great Again
+            </p>
+            <p className="text-muted">
+              Espor severler tarafından espor severler için yapıldı.
+            </p>
+          </Col>
+          <Col xl={4} md={4} sm={4} xs={4} className="signButtonCol">
+            <Button variant="primary">Şimdi Kaydol</Button>
+          </Col>
+        </Col>
+      </Row>
+      {/* footer social media links */}
+      <Row>
+        <Col xl={8} md={8} sm={8} xs={8} className="footerSocialMediaCol mt-4">
+          <Col xl={8} md={8} sm={8} xs={8}><a className="text-muted" href="/">Aydınlatma Metni</a></Col>
+        <Col xl={4} md={4} sm={4} xs={4} className="footerSocialMedia"><a href="/"><FaInstagram/></a><a href="/"><FaTwitter/></a></Col>
+        </Col>
+      </Row>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
