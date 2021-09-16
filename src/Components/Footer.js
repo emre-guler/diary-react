bd0f68897d1342534c470680927b5167d5e1a59a
Oguz Ozsen <aslanoguz_1101@hotmail.com>
2021-09-16 13:21:19 +0300

    footer responsive

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button, Row, Col } from "react-bootstrap";
-import {FaInstagram, FaTwitter} from "react-icons/fa"
+import { FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="Footer">
       {/* footer logo */}
       <Row className="footerLogoRow">
-        <Col xl={8} md={8} sm={8} xs={8}>
+        <Col xl={8} md={10} sm={11} xs={11}>
           <Col>
             <a href="/" className="footerLogo">
               Ionic Diary
@@ -17,29 +17,29 @@ const Footer = () => {
       </Row>
       {/* footer links */}
       <Row>
-        <Col xl={8} md={8} sm={8} xs={8} className="footerLinksCol">
-          <Col xl={3} md={3} sm={3} xs={3}>
+        <Col xl={8} md={10} sm={11} xs={11} className="footerLinksCol">
+          <Col xl={3} md={3} sm={6} xs={12}>
             <h5>Ürünler</h5>
             <a href="/">Lorem</a>
             <a href="/">Ipsum Dolor</a>
             <a href="/">Dolor Sit Amet</a>
             <a href="/">Sova</a>
           </Col>
-          <Col xl={3} md={3} sm={3} xs={3}>
-            <h5>Scoutli Hakkında</h5>
+          <Col xl={3} md={3} sm={6} xs={12}>
+            <h5>Ionic Hakkında</h5>
             <a href="/">Lorem</a>
             <a href="/">Ipsum Dolor</a>
             <a href="/">Dolor Sit Amet</a>
             <a href="/">Sova</a>
           </Col>
-          <Col xl={3} md={3} sm={3} xs={3}>
+          <Col xl={3} md={3} sm={6} xs={12}>
             <h5>Destek</h5>
             <a href="/">Lorem</a>
             <a href="/">Ipsum Dolor</a>
             <a href="/">Dolor Sit Amet</a>
             <a href="/">Sova</a>
           </Col>
-          <Col xl={3} md={3} sm={3} xs={3}>
+          <Col xl={3} md={3} sm={6} xs={12}>
             <h5>İletişim</h5>
             <a href="/">Lorem</a>
             <a href="/">Ipsum Dolor</a>
@@ -50,25 +50,36 @@ const Footer = () => {
       </Row>
       {/* footer copyright */}
       <Row>
-        <Col xl={8} md={8} sm={8} xs={8} className="footerCopyrightCol">
-          <Col xl={8} md={8} sm={8} xs={8} className="footerCopyright">
+        <Col xl={8} md={10} sm={11} xs={11} className="footerCopyrightCol">
+          <Col xl={8} md={8} sm={12} xs={12} className="footerCopyright">
             <p className="text-muted">
-              © 2021 Scoutli. Make Esports Great Again
+              © 2021 IONIC. Immortalize your day.
             </p>
             <p className="text-muted">
-              Espor severler tarafından espor severler için yapıldı.
+              Teknoloji severler tarafından günlük tutmayı sevenler için yapıldı.
             </p>
           </Col>
-          <Col xl={4} md={4} sm={4} xs={4} className="signButtonCol">
+          <Col xl={4} md={4} sm={12} xs={12} className="signButtonCol">
             <Button variant="primary">Şimdi Kaydol</Button>
           </Col>
         </Col>
       </Row>
       {/* footer social media links */}
       <Row>
-        <Col xl={8} md={8} sm={8} xs={8} className="footerSocialMediaCol mt-4">
-          <Col xl={8} md={8} sm={8} xs={8}><a className="text-muted" href="/">Aydınlatma Metni</a></Col>
-        <Col xl={4} md={4} sm={4} xs={4} className="footerSocialMedia"><a href="/"><FaInstagram/></a><a href="/"><FaTwitter/></a></Col>
+        <Col xl={8} md={10} sm={11} xs={11} className="footerSocialMediaCol mt-4">
+          <Col xl={8} md={8} sm={12} xs={12}>
+            <a className="text-muted" href="/">
+              Aydınlatma Metni
+            </a>
+          </Col>
+          <Col xl={4} md={4} sm={12} xs={12} className="footerSocialMedia">
+            <a href="/">
+              <FaInstagram />
+            </a>
+            <a href="/">
+              <FaTwitter />
+            </a>
+          </Col>
         </Col>
       </Row>
     </footer>
